dd1048d53cefc8137b00c163bb29167838a809a8
hadoizz <82988056+hadoizz@users.noreply.github.com>
2024-09-27 20:56:47 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",  // Set the base to the root of your domain
   plugins: [vue()],
   resolve: {
     extensions: [".vue", ".js"],
@@ -11,8 +12,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
       "~": path.resolve(__dirname, "./node_modules"),
     },
-  },
-  build: {
-    outDir: "dist",  // Output directory
   },
 });
