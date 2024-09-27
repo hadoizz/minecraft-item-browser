c7b591a0697d85ccc9d5174b7ebca0448661014e
hadoizz <82988056+hadoizz@users.noreply.github.com>
2024-09-27 20:59:19 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import McHome from "@/views/McHome.vue";
-import NotFound from "@/views/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),  // No base path, uses the root
   routes: [
     {
       path: '/',
-      redirect: { name: 'Home' }
+      redirect: { name: 'Home' },
     },
     {
       path: "/:item_name?",
       name: "Home",
       component: McHome,
       props: true,
-    },
-    // Catch-all route for 404
-    {
-      path: '/:catchAll(.*)',
-      name: 'NotFound',
-      component: NotFound,
     },
   ],
 });
