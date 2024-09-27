deee01f284042ac4c9dbcdb810b9efcac09631d3
hadoizz <82988056+hadoizz@users.noreply.github.com>
2024-09-27 21:14:14 +0300

    Update sitemap.mjs

--- scripts/sitemap.mjs
+++ scripts/sitemap.mjs
@@ -2,15 +2,13 @@ import { SitemapStream } from "sitemap";
 import { readFileSync, createWriteStream } from "fs";
 import path from "path";
 
-// Determine base URL for production or local development
-const BASE_URL = process.env.NODE_ENV === "production"
-  ? "https://minecraft-item.weplayold.com"
-  : "http://localhost:8080";
+// Set base URL for production only
+const BASE_URL = "https://minecraft-item.weplayold.com";
 
 // Define output path for sitemap.xml
 const OUTPUT_PATH = path.resolve(process.cwd(), "public/sitemap.xml");
 
-// Read items from the JSON file
+// Read items from JSON file
 const items = JSON.parse(readFileSync(path.resolve(process.cwd(), "public/js/items.json"), "utf-8"));
 console.log(`Number of items: ${items.length}`);
 
